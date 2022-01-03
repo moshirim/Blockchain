71ed7fa6d0ea0dd1fc96f03baf833a0c6c0a06a1
Mahyar Moshiri <66429937+moshirim@users.noreply.github.com>
2022-01-03 06:47:05 -0600

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -22,4 +22,8 @@ contract SimpleStorage {
     function retrieve() public view returns (uint256){
         return favoriteNumber;
     }
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
 }
